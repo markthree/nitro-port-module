476292c177f027e7d1c0a9e670d8c7c20aadc966
markthree <1801982702@qq.com>
2024-02-05 21:48:27 +0800

    feat: add polyfill option

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
 import { defu } from "defu";
-import type { NitroModule } from "nitropack";
+import type { Nitro, NitroModule } from "nitropack";
+import { isFunction, isNumber, isString } from "m-type-tools";
+
+type PolyfillFn = (nitro: Nitro, port: number) => string;
 
 interface Options {
   /**
    * @default 3000
    */
   port?: number;
+  /**
+   * @default Default automatic detection
+   */
+  polyfill?: string | PolyfillFn;
 }
 
 const defaultOptions: Options = {
   port: 3000,
+  polyfill(nitro, port) {
+    return nitro.options.preset.includes("deno")
+      ? `Deno.env.set("PORT", "${port}")`
+      : `process.env.PORT = '${port}'`;
+  },
 };
 
 function nitroPort(options = defaultOptions): NitroModule {
@@ -17,7 +29,17 @@ function nitroPort(options = defaultOptions): NitroModule {
     name: "nitro-port",
     setup(nitro) {
       const { preset, dev } = nitro.options;
-      const { port } = options = defu(options, defaultOptions);
+      const { port, polyfill } = options = defu(options, defaultOptions);
+      const logger = nitro.logger.withTag("nitro");
+      if (!isNumber(port)) {
+        logger.error("port must be a number");
+        return;
+      }
+
+      if (!isFunction(polyfill) && !isString(polyfill)) {
+        logger.error("polyfill must be a function or string");
+        return;
+      }
 
       if (port === 3000) {
         // default is 3000, so we don't need to do anything
@@ -29,12 +51,8 @@ function nitroPort(options = defaultOptions): NitroModule {
         return;
       }
 
-      const logger = nitro.logger.withTag("nitro");
       nitro.options.rollupConfig ??= { output: {} };
       const plugins = nitro.options.rollupConfig.plugins ??= [];
-      const polyfill = preset.includes("deno")
-        ? `Deno.env.set("PORT", "${port}")`
-        : `process.env.PORT = '${port}'`;
 
       const files = {
         "bun": "index.mjs",
@@ -53,7 +71,12 @@ function nitroPort(options = defaultOptions): NitroModule {
 
       function usePolyfill(fileName: string) {
         if (files[preset] === fileName) {
-          return polyfill;
+          if (isString(polyfill)) {
+            return polyfill;
+          }
+          if (isFunction(polyfill)) {
+            return polyfill(nitro, port!);
+          }
         }
         return "";
       }
